6479d20d8ff9364646ccb104a9ce1d5b4b1d0fe7
xadd <alexander.kozlovich@gmail.com>
2017-10-06 13:21:43 +0300

    1.0.1

--- index.js
+++ index.js
@@ -23,28 +23,38 @@ function streamString(strings) {
         var item = isValue ? args[1 + offset >> 1] : strings[offset >> 1]
         offset++
 
-        if (!item) {
-            next()
-            return
-        }
+        if (isValue) {
+            if (item === null) {
+                result.write('null', 'utf8', next)
+                return
+            }
 
-        if (!isValue) {
+            if (item === undefined) {
+                result.write('undefined', 'utf8', next)
+                return
+            }
+
+            if (item instanceof Buffer) {
+                result.write(item, next)
+                return
+            }
+
+            if (item instanceof stream.Readable) {
+                item.pipe(result, { end: false })
+                item.on('end', next)
+                return
+            }
+
+            result.write(item.toString(), 'utf8', next)
+        }
+        else {
+            if (!item) {
+                next()
+                return
+            }
+
             result.write(item, 'utf8', next)
-            return
         }
-
-        if (item instanceof Buffer) {
-            result.write(item, next)
-            return
-        }
-
-        if (item instanceof stream.Readable) {
-            item.pipe(result, { end: false })
-            item.on('end', next)
-            return
-        }
-
-        result.write(item.toString(), 'utf8', next)
     }
 
     return result
